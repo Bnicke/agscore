c396e2512a22208bf0cde0d497d6970410c32b2b
Bnicke <niklas@bergstrom.se>
2016-04-24 16:15:31 +0200

    Delete start list item

--- vendor/agscore/agscore.js
+++ vendor/agscore/agscore.js
@@ -32,76 +32,6 @@ function isNumber(value) {
     }
     return !isNaN(value - 0);
 }
-function agupdate(part,index,agtable) {
-    //var post = data[index];
-    var post = JSON.parse("{}");
-    post.id = data[index].id;
-    if ( $(window).width() > 900 ) {
-        table = agtable;
-        hidetable = agtable + '-stacktable-small-only';
-    } else {
-        table = agtable + '-stacktable-small-only';
-        hidetable = agtable;
-    }
-	var gymnast = $("#" + table + " :input[id='gymnast_"+index+"']").val();
-	var number = $("#" + table + " :input[id='number_"+index+"']").val();
-	var born = $("#" + table + " :input[id='born_"+index+"']").val();
-	var team = $("#" + table + " :input[id='team_"+index+"']").val();
-	var klass = $("#" + table + " :input[id='class_"+index+"']").val();
-	var rules = $("#" + table + " :input[id='rules_"+index+"']").val();
-	var pool = $("#" + table + " :input[id='pool_"+index+"']").val();
-	var about = $("#" + table + " :input[id='about_"+index+"']").val();
-	var currvalue = $("#" + table + " :input[id='"+part+"_"+index+"']").val();
-	var id = camelize(gymnast + " " + team);
-	if (isNumber(number) && (id.length > 6) && (camelize(team).length > 1) && (klass.length > 1) && (pool.length > 0)) {
-		post.gymnast = gymnast;
-		post.number = number;
-		post.born = born;
-		post.team = team;
-		post.class = klass;
-		post.rules = rules;
-		post.pool = pool;
-		post.about = about;
-		post.id = id;
-    		var notify;
-    		var notifytype;
-    		$.ajax({
-		        'async': false,
-		        type: "POST",
-		        url: "/ES/comp1/startList/" + id,
-		        data: JSON.stringify(post),
-		        contentType: "application/json; charset=utf-8",
-		        dataType: "json",
-		        success: function(s){
-		                notify = "Gymnast " + gymnast + " updated!";
-		                notifytype = "success";
-                	},
-        		error: function(xhr, ajaxOptions, thrownError) {
-                		notify = "\"" + xhr.status + " " + thrownError + "\" when updating " + gymnast;
-                		notifytype = "error";
-        		}
-    		});
-                post.gymnast = startlistedit(post.id,"gymnast",post.gymnast,table);
-                post.number = startlistedit(post.id,"number",post.number,table);
-                post.team = startlistedit(post.id,"team",post.team,table);
-                post.pool = startlistedit(post.id,"pool",post.pool,table);
-                post.born = startlistedit(post.id,"born",post.born,table);
-                post.class = startlistedit(post.id,"class",post.class,table);
-                post.rules = startlistedit(post.id,"rules",post.rules,table);
-                post.about = startlistedit(post.id,"about",post.about,table);
-                post.stored = '<label><span></span><span class="switch switch-6"><input onchange="clearscore(\'' + post.id + '\',\'' + post.id + '\',\'' + table + '\',\'' + post.id + '\');" type="checkbox" id="check_' + post.id + '" name="check_' + post.id + '" value="PH" /><span></span></span></label>';
-
-		data.unshift(post);
-		var tmp=data[0];
-		data[0]=data[1];
-		data[1]=tmp;
-		setTimeout(function() {processingComplete("#" + table);}, 100);
-  		dynatable.records.updateFromJson({records: data});
-  		dynatable.records.init();
-  		dynatable.process();
-		$.mobility.notify(notify,notifytype);	
-	}
-}
 function agcalculate(part,number,agtable,index) {
     //var post=data[index];
     var post = JSON.parse("{}");
@@ -298,24 +228,44 @@ function inputreg (index,part,agnumber,agvalue,agtable) {
 		return '<input class="form-control" min="0" max="15" onchange="agcalculate(\''+ part + '\',\'' + agnumber + '\',\'' + agtable +'\',\'' + index +'\');" type="number" step="any" id="' + part + '_' + agnumber + '" name="' + part + '_' + agnumber + '" value="' + agvalue + '">';
 	}
 }
+function startlistedit (action,post,notify) {
+if (action == "POST" ) {
+                $.ajax({
+                        'async': false,
+                        type: "POST",
+                        url: "/ES/comp1/startList/" + post.id,
+                        data: JSON.stringify(post),
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function(s){
+                                notifytxt = "Gymnast " + post.gymnast + " updated!";
+                                notifytype = "success";
+                        },
+                        error: function(xhr, ajaxOptions, thrownError) {
+                                notify = "\"" + xhr.status + " " + thrownError + "\" when updating " + post.gymnast;
+                                notifytype = "error";
+                        }
+                });
+} else if (action == "DELETE" ) {
+                $.ajax({
+                    'async': false,
+                    type: "DELETE",
+                    url: "/ES/comp1/startList/" + post.id,
+                    success: function(s){
+                                notifytxt = "Gymnast " + post.gymnast + " deleted!";
+                                notifytype = "success";
+                    },
+                        error: function(xhr, ajaxOptions, thrownError) {
+                                notifytxt = "\"" + xhr.status + " " + thrownError + "\" when deleting " + post.gymnast;
+                                notifytype = "error";
+                        }
 
-function startlistedit (index,part,agvalue,agtable) {
-	 if (agvalue === undefined) {
-	 	var value="";
-		agvalue="";
-	} else {
-	 	var value='value="' + agvalue + '"';
-	}
-	if (part == "gymnast") {
-		var placeholder='placeholder="New.."';
-	} else if ((part == "team") || (part == "class") || (part == "rules")){
-		var placeholder='placeholder="Start typing.."';
-	} 
-	if (part == "about") {
-		return '<textarea onchange="agupdate(\'' + part + '\',\'' + index + '\',\'' + agtable + '\');" id="' + part + '_' + index + '" class="form-control" rows="2" placeholder="Enter Text">' + agvalue + '</textarea>';
-	} else {
-		return '<input onchange="agupdate(\'' + part + '\',\'' + index + '\',\'' + agtable + '\');" id="' + part + '_' + index + '" type="text" class="form-control" ' + placeholder + '" ' + value + ' autocomplete="off" />';
-	}
+                });
+}
+if (notify) {
+        $.mobility.notify(notifytxt,notifytype);
+}
+
 }
 function draweditable(table,type) {
 	var datalocal = [];
