57cccd844b5e3564db85f69ee65c21d8e2d102e4
Niklas Bergstrom <niklas@bergstrom.se>
2017-05-05 13:34:34 +0200

    Fix

--- oauth2_proxy/update_users.sh
+++ oauth2_proxy/update_users.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 while true; do
-	curl -s eshost:9200/bin/admin/users |  python -c "import sys, json; a = json.load(sys.stdin); print(a['_source']['users'])" > /authenticatedemailsfile
-	echo niklas.bergstrom.se@gmail.com >> /authenticatedemailsfile
+	curl -s eshost:9200/bin/admin/users |  python -c "import sys, json; print('niklas.bergstrom.se@gmail.com'); a = json.load(sys.stdin); print(a['_source']['users'])" | sort -u > /authenticatedemailsfile
 	sleep 60
 done
