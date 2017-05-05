3f1f1feb002c59a1635b02c092fe6c1e1bbefbe2
Niklas Bergstrom <niklas@bergstrom.se>
2017-05-05 14:08:35 +0200

    Typo

--- oauth2_proxy/update_users.sh
+++ oauth2_proxy/update_users.sh
@@ -3,7 +3,7 @@ while true; do
 	sleep 60
 	curl -s eshost:9200/bin/admin/users |  python -c "import sys, json; print('niklas.bergstrom.se@gmail.com'); a = json.load(sys.stdin); print(a['_source']['users'])" | sort -u > /tmp/authenticatedemailsfile
         diff /tmp/authenticatedemailsfile /authenticatedemailsfile
-        if [ $? -ne 1 ]; then 
+        if [ $? -ne 0 ]; then 
 		cat /tmp/authenticatedemailsfile > /authenticatedemailsfile
 	fi
 done
