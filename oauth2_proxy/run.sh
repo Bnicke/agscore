c721c22a9bc2eca847984b633dec186a5ac9b5a7
Niklas Bergstrom <niklas@bergstrom.se>
2017-05-05 10:01:13 +0200

    variable

--- oauth2_proxy/run.sh
+++ oauth2_proxy/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-if [ $authenticatedemailsfile ]; then
-	echo $authenticatedemailsfile | base64 -d > /authenticatedemailsfile
-	/usr/share/oauth2_proxy/oauth2_proxy "$@" -authenticated-emails-file=/authenticatedemailsfile
-else
+if [ -z ${authenticatedemailsfile+x} ]; then 
+	echo "authenticatedemailsfile is unset" 
 	/usr/share/oauth2_proxy/oauth2_proxy "$@"
+else 
+	echo "authenticatedemailsfile is set to '$authenticatedemailsfile'"
+	echo $authenticatedemailsfile | base64 -d > /authenticatedemailsfile
+	/usr/share/oauth2_proxy/oauth2_proxy -authenticated-emails-file=/authenticatedemailsfile "$@"
 fi
