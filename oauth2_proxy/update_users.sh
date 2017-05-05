af131acfcaf959d699280b2ee51ca66f487c2c26
Niklas Bergstrom <niklas@bergstrom.se>
2017-05-05 13:59:51 +0200

    logic

--- oauth2_proxy/update_users.sh
+++ oauth2_proxy/update_users.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while true; do
+	sleep 60
 	curl -s eshost:9200/bin/admin/users |  python -c "import sys, json; print('niklas.bergstrom.se@gmail.com'); a = json.load(sys.stdin); print(a['_source']['users'])" | sort -u > /tmp/authenticatedemailsfile
         diff /tmp/authenticatedemailsfile /authenticatedemailsfile
         if [ $? -ne 1 ]; then 
 		cat /tmp/authenticatedemailsfile > /authenticatedemailsfile
 	fi
-	sleep 60
 done
