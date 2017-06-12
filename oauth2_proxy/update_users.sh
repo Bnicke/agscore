bc201313f3079884e7f2561d238f747d1f0dd606
Bnicke <niklas@bergstrom.se>
2017-06-12 23:27:37 +0200

    cp

--- oauth2_proxy/update_users.sh
+++ oauth2_proxy/update_users.sh
@@ -4,6 +4,6 @@ while true; do
 	curl -s eshost:9200/bin/admin/users |  python -c "import sys, json; print('niklas.bergstrom.se@gmail.com'); a = json.load(sys.stdin); print(a['_source']['users'])" | sort -u > /tmp/authenticatedemailsfile
         diff /tmp/authenticatedemailsfile /authenticatedemailsfile
         if [ $? -ne 0 ]; then 
-		cat /tmp/authenticatedemailsfile > /authenticatedemailsfile
+		\cp /tmp/authenticatedemailsfile /authenticatedemailsfile
 	fi
 done
